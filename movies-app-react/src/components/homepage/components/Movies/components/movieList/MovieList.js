b9e8cd8755076ea7e12965427c306afc54ddb3c8
VAleksandrov <Vasil_Aleksandrov@epam.com>
2021-10-06 11:32:49 +0300

    fixed css for align movies on one line

--- movies-app-react/src/components/homepage/components/Movies/components/movieList/MovieList.js
+++ movies-app-react/src/components/homepage/components/Movies/components/movieList/MovieList.js
@@ -5,7 +5,8 @@ function MovieList(props) {
     return (
         <>
             <h5 className="movies_count"><span className="counter">{props.movies.length}</span> movies found</h5>
-            {props.movies.map((movie, index) => (
+            <main className="movie_list_container">
+                 {props.movies.map((movie, index) => (
                 <Movie key={index} cover={movie.cover}
                 title={movie.title}
                 genre={movie.genre}
@@ -17,6 +18,8 @@ function MovieList(props) {
                 toggleDeleteMovieModal={props.toggleDeleteMovieModal}>
                 </Movie>
             ))}
+            </main>
+           
         </>
     )
 }
