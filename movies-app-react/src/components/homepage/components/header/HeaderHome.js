35f5dfee9fe00aa89b3e42588d51cffe63a5e5ba
VAleksandrov <Vasil_Aleksandrov@epam.com>
2021-10-10 00:04:18 +0300

    added Memo for callback

--- movies-app-react/src/components/homepage/components/header/HeaderHome.js
+++ movies-app-react/src/components/homepage/components/header/HeaderHome.js
@@ -2,15 +2,8 @@ import "./HeaderHome.css";
 import React, { useEffect, useState } from "react";
 
 function HomeHeader(props) {
-  // let movie = props.choosenMovie;
-  // const movieHandler = props.selectedMovie;
-  // console.log(props)
-  // const [selectedMovie, movieHandler] = useState(null);
   let showDetails = Object.keys(props.choosenMovie).length > 0;
-  useEffect(() => {
-    console.log("useEfect");
-    console.log(showDetails);
-  }, [props.choosenMovie]);
+  useEffect(() => {}, [props.choosenMovie]);
 
   let headerContent = (
     <>
@@ -38,19 +31,37 @@ function HomeHeader(props) {
   let movieDetaisContent = (
     <>
       <section className="movie_details">
-      <h4 className="prime_text">
-        <span>netflix</span>roulette
-      </h4>
-      <img src={`./${props.choosenMovie.cover}.png`} alt="cover"></img>
+        <h4 className="prime_text">
+          <span>netflix</span>roulette
+        </h4>
+        <img
+          className="image_details"
+          src={`./${props.choosenMovie.cover}.png`}
+          alt="cover"
+        ></img>
+        <h1>{props.choosenMovie.title}</h1>
+        <h6>{props.choosenMovie.genre}</h6>
+        <div className="year_duration_wrapper">
+          <h4>{props.choosenMovie.year}</h4>
+          <h4 className="runtime">{props.choosenMovie.runtime}</h4>
+        </div>
+        <h2>{props.choosenMovie.rating}</h2>
+        <p>{props.choosenMovie.description}</p>
+        <div className="searchIcon">
+          <img src={"./Oval.png"} alt="searchIcon"></img>
+          <img src={"./Line_2.png"} alt="searchIcon"></img>
+        </div>
       </section>
     </>
   );
 
   return (
     <>
-      <header className="container">{showDetails ? movieDetaisContent : headerContent}</header>
+      <header className="container">
+        {showDetails ? movieDetaisContent : headerContent}
+      </header>
     </>
   );
 }
 
-export default HomeHeader;
+export default React.memo(HomeHeader);
